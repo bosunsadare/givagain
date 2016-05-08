f23b57ad87ae9646722cf9c6b2f7f5b4c1f3d86a
Bosun Sadare <Bosun.Sadare@aspectworld.com>
2016-05-08 22:09:49 +0100

    Further development: giving page

--- js/givagain.js
+++ js/givagain.js
@@ -10,4 +10,22 @@ $(function () {
 
   $( "[data-role='footer']" ).toolbar({ theme: "a" });
 
+  var charities = new XMLHttpRequest();
+  var url = "js/charities.json";
+
+  charities.open("GET", url, true);
+  charities.send();
+
+  charities.onreadystatechange = function() {
+    if (charities.readyState === 4 && charities.status === 200 ) {
+      var charityLister = JSON.parse(charities.responseText);
+      var charityList = charityLister['Charities'];
+    }
+    var i;
+    var output = '<option value="" disabled selected>Select Charity</option>';
+    for(i = 0; i < charityList.length; i++) {
+      output += '<option value="' + charityList[i].name + '">' + charityList[i].description + '</option>';
+    }
+    $('#charities').html(output);
+  }
 });
